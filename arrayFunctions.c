c3e910231f3726fc7d5663535cf12b8db6c08414
mariam <136911996+mirmariam@users.noreply.github.com>
2024-11-24 11:49:50 +0530

    Add files via upload

---
+++ arrayFunctions.c
@@ -0,0 +1,129 @@
+//Array functions
+
+#include<stdio.h>
+
+int create(int arr[]);
+void display(int arr[], int n, int flag);
+void search(int arr[], int n);
+int insertion(int arr[],int n);
+void deletion(int arr[], int n);
+
+int main() {
+
+  int arr[100], n;
+
+  n = create(arr);
+  display(arr,n,0);
+  search(arr,n);
+  n = insertion(arr,n);
+  deletion(arr,n);
+
+  return 0;
+}
+
+int create(int arr[]) {
+
+  int i, n;
+
+  printf("Enter the length of the array: ");
+  scanf("%d", &n);
+
+  for(i = 0;i < n; i++) {
+    printf("Enter the element %d: ",(i+1));
+    scanf("%d", &arr[i]);
+  }
+
+  return n;
+}
+
+void display(int arr[], int n, int flag) {
+    int i;
+    if(flag==0) {
+    printf("The elements of the array are:\n");
+    } else {
+    printf("The elements of the new array are:\n");
+    }
+    for (i = 0;i < n; i++) {
+    printf("%d ",arr[i]);
+    }
+}
+
+void search(int arr[], int n) {
+    
+    int i, flag = 0, x;
+  
+    printf("\nEnter the element to search for: ");
+    scanf("%d", &x);
+    for(i = 0;i < n; i++) {
+        if(arr[i] == x) {
+        printf("The element was found at position %d.\n", (i+1));
+        flag = 1;
+        }
+    }
+  
+    if (flag == 0) {
+        printf("The element was not found.\n");
+    }
+}
+
+int insertion(int arr[], int n) {
+   
+    int i;
+
+    //Insertion at start
+    for (i = n; i > 0; i--) {
+        arr[i]=arr[(i - 1)];
+    }
+    n++;
+    printf("Enter the element at start: ");
+    scanf("%d", &arr[0]);
+    display(arr, n, 1);
+
+    //Insertion at end
+    printf("\nEnter the element at end: ");
+    scanf("%d", &arr[n]);
+    n++;
+    display(arr, n, 1);
+
+    //Insertion at n
+    int p;
+    printf("\nEnter the position where you want to add an element: ");
+    scanf("%d", &p);
+    for(i = n; i >= p; i--) {
+        arr[i] = arr[(i - 1)];
+    }
+    printf("Enter the element at %d: ", p);
+    scanf("%d", &arr[(p - 1)]);
+    n++;
+    display(arr, n, 1);
+
+    return n;
+}
+
+void deletion(int arr[], int n) {
+    int i;
+    //Deletion at start
+    for(i = 0;i < (n-1); i++) {
+        arr[i] = arr[(i + 1)];
+    }  
+    n--;
+    printf("\n//Element at start deleted//\n");
+    display(arr, n, 1);
+
+    //Deletion at end
+    arr[(n - 1)] = arr[(n + 50)];
+    n--;
+    printf("\n//Element at end deleted//\n");
+    display(arr, n, 1);
+
+    //Deletion at n
+    int p;
+    printf("\nEnter the position where you want to delete an element: ");
+    scanf("%d", &p);
+    for (i = (p - 1);i < (n - 1); i++) {
+        arr[i] = arr[(i + 1)]; 
+    }
+    n--;
+    printf("\n//Element at %d deleted//\n", p);
+    display(arr, n, 1);
+}
---
+++ removeDuplicatesLL.c
@@ -0,0 +1,97 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Definition of a node
+struct Node {
+    int data;
+    struct Node *next;
+};
+
+// Function to create a new node
+struct Node* createNode(int data) {
+    struct Node *newNode = (struct Node*)malloc(sizeof(struct Node));
+    if (!newNode) {
+        printf("Memory allocation failed\n");
+        return NULL;
+    }
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode;
+}
+
+// Function to insert a new node at the end of the list
+void insertNode(struct Node **head, int data) {
+    struct Node *newNode = createNode(data);
+    if (*head == NULL) {
+        *head = newNode;
+    } else {
+        struct Node *temp = *head;
+        while (temp->next != NULL) {
+            temp = temp->next;
+        }
+        temp->next = newNode;
+    }
+}
+
+// Function to print the list
+void printList(struct Node *head) {
+    if (head == NULL) {
+        printf("The list is empty.\n");
+        return;
+    }
+    struct Node *temp = head;
+    while (temp != NULL) {
+        printf("%d -> ", temp->data);
+        temp = temp->next;
+    }
+    printf("NULL\n");
+}
+
+// Function to remove duplicates from the linked list
+void removeDuplicates(struct Node *head) {
+    struct Node *current = head;
+    struct Node *prev = NULL;
+    struct Node *temp = NULL;
+
+    while (current != NULL && current->next != NULL) {
+        prev = current;
+        temp = current->next;
+
+        // Check for duplicates
+        while (temp != NULL) {
+            if (current->data == temp->data) {
+                prev->next = temp->next;
+                free(temp);
+                temp = prev->next;
+            } else {
+                prev = temp;
+                temp = temp->next;
+            }
+        }
+        current = current->next;
+    }
+}
+
+int main() {
+    struct Node *head = NULL;
+    int n, data;
+
+    printf("Enter the number of elements in the list: ");
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) {
+        printf("Enter data for node %d: ", i + 1);
+        scanf("%d", &data);
+        insertNode(&head, data);
+    }
+
+    printf("\nOriginal List:\n");
+    printList(head);
+
+    removeDuplicates(head);
+
+    printf("\nList after removing duplicates:\n");
+    printList(head);
+
+    return 0;
+}
